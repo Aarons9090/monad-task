18e7b6d0a1e33edf65b540819150f3bac42f9fb1
Aaron Hirvi <aaron.hirvi@tuni.fi>
2022-11-20 22:23:26 +0200

    edit dodging of other planes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from dotenv import dotenv_values
 import requests
 import webbrowser
 import websocket
@@ -6,6 +5,7 @@ import json
 import time
 import math
 import numpy as np
+from dotenv import dotenv_values
 
 FRONTEND_BASE = "noflight.monad.fi"
 BACKEND_BASE = "noflight.monad.fi/backend"
@@ -15,6 +15,7 @@ game_id = None
 
 def normalize_heading(heading):
     return round(heading + 360) % 360
+
 
 def on_message(ws: websocket.WebSocketApp, message):
     [action, payload] = json.loads(message)
@@ -54,6 +55,29 @@ def calculate_distance(x1,x2,y1,y2):
     return dist
 
 
+def calculate_new_direction(old_dir, target_dir):
+    # Adjust bearing accordingly. (There is probably a better way for this
+    # but it works)
+    if old_dir - target_dir < 180:
+        if abs(old_dir - target_dir) < 20:
+            new_dir = target_dir
+        else:
+            if old_dir > target_dir:
+                new_dir = old_dir - 20
+            else:
+                new_dir = old_dir + 20
+    else:
+        if abs(old_dir - target_dir) < 20:
+            new_dir = target_dir
+        else:
+            if old_dir > target_dir:
+                new_dir = old_dir + 20
+            else:
+                new_dir = old_dir - 20
+
+    return new_dir
+
+
 # Change this to your own implementation
 def generate_commands(game_state):
     commands = []
@@ -62,13 +86,19 @@ def generate_commands(game_state):
         x = aircraft["position"]["x"]
         y = aircraft["position"]["y"]
 
-        airport = game_state["airports"][0]
+        # Find correct destination
+        # this is a slow and inefficient way and could probably be done better
+        for a in game_state["airports"]:
+            if a["name"] == aircraft["destination"]:
+                airport = a
+
         airport_x = airport["position"]["x"]
         airport_y = airport["position"]["y"]
         airport_dir = airport["direction"]
 
+        # offsets for target
         y_offset_opposite = 20
-        y_offset_directional = 23
+        offset_directional = 23
 
         # if the plane is inside airport, match directions
         if calculate_distance(x, airport_x, y, airport_y) < 25:
@@ -80,31 +110,24 @@ def generate_commands(game_state):
                 airport_y -= y_offset_opposite
             else:
                 # offset to same direction airport is pointing to
-                airport_y += int(y_offset_directional * math.sin(np.deg2rad(abs(airport_dir + 180))))
-                airport_x += int(y_offset_directional * math.cos(np.deg2rad(abs(airport_dir + 180))))
+                airport_y += int(offset_directional * np.sin(np.deg2rad(abs(airport_dir + 180))))
+                airport_x += int(offset_directional * np.cos(np.deg2rad(abs(airport_dir + 180))))
 
             target_dir = calculate_direction(x, airport_x, y, airport_y)
 
-        old_dir = aircraft["direction"]
+        for i in game_state["aircrafts"]:
+            if i["id"] == aircraft["id"]:
+                continue
 
-        # Adjust bearing accordingly. (There is probably a better way for this
-        # but it works)
-        if old_dir - target_dir < 180:
-            if abs(old_dir - target_dir) < 20:
-                new_dir = target_dir
-            else:
-                if old_dir > target_dir:
-                    new_dir = old_dir - 20
-                else:
-                    new_dir = old_dir + 20
-        else:
-            if abs(old_dir - target_dir) < 20:
-                new_dir = target_dir
-            else:
-                if old_dir > target_dir:
-                    new_dir = old_dir + 20
-                else:
-                    new_dir = old_dir - 20
+            distance_to_other = calculate_distance(x, i["position"]["x"], y, i["position"]["y"])
+
+            # if planes are close to collision, change target to dodge
+            if distance_to_other < 60:
+                target_dir -= 25
+
+        # calculate new direction
+        old_dir = aircraft["direction"]
+        new_dir = calculate_new_direction(old_dir, target_dir)
 
         commands.append(f"HEAD {aircraft['id']} {normalize_heading(new_dir)}")
 
